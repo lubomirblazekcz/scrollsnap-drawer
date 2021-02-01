a6a7778dac5c91d12cc6b949c1fdd045f22f366b
Lubomír Blažek <evromalarkey@gmail.com>
2021-02-01 19:54:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+import { Application, Controller } from "https://cdn.jsdelivr.net/npm/stimulus@2.0.0/+esm";
+
+const Stimulus = new Application(document.documentElement, {
+    controllerAttribute: "data-controller",
+    actionAttribute: "data-action",
+    targetAttribute: "data-ref"
+});
+
+Controller.prototype.getTarget = function(target) {
+    return this.targets.find(target)
+};
+
+Stimulus.start();
+
+Stimulus.register("lib-drawer", class extends Controller {
+    connect() {
+        console.log("test");
+        setTimeout(() => this.getTarget("nav").classList.add("is-transition"), 50);
+        this.getTarget("nav").addEventListener("click", (e) => {
+            if (e.target === this.getTarget("nav")) {
+                this.hide();
+            }
+        })
+    }
+
+    show(e) {
+        this.getTarget("nav").scrollLeft = 0;
+        this.getTarget("nav").style.setProperty('--drawerOpacity', `1`);
+        this.getTarget("nav").classList.add("is-opacity", "state-active");
+        document.documentElement.classList.add("drawer-active");
+    }
+
+    hide() {
+        this.getTarget("nav").classList.remove("state-active");
+        this.getTarget("nav").classList.add("is-opacity");
+        this.getTarget("nav").style.setProperty('--drawerOpacity', `0`);
+        document.documentElement.classList.remove("drawer-active");
+    }
+
+    scroll(e) {
+        if (e.target.scrollLeft > 1) {
+            this.getTarget("nav").classList.remove("is-opacity");
+            this.getTarget("nav").style.setProperty('--drawerOpacity', `${Math.abs((e.target.scrollLeft / this.getTarget("nav").children[0].clientWidth) - 1)}`);
+        }
+
+        if (e.target.scrollLeft === this.getTarget("nav").children[0].clientWidth) {
+            this.getTarget("nav").classList.remove("state-active");
+            document.documentElement.classList.remove("drawer-active");
+        }
+    }
+});
